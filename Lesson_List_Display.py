c1773ffdf5f2299fe3c1d0695821082df5ad1955
kriyanation <homeofmars@gmail.com>
2020-05-29 23:59:16 +0530

    MVP 1.1-Styles

--- Lesson_List_Display.py
+++ Lesson_List_Display.py
@@ -7,15 +7,15 @@ class MagicLessonList(tk.Toplevel):
     def __init__(self,parent,*args, **kwargs):
         super().__init__(parent,*args, **kwargs)
         s = ttk.Style(self)
-        s.configure('TScrollbar', background="gray27", foreground="white")
-        s.map('TScrollbar', background=[('active', '!disabled', 'gray27'), ('pressed', "white")],
-              foreground=[('pressed', "white"), ('active', "gray27")])
-        self.configure(background='gray25')
-        self.l_canvas = tk.Canvas(self, background='gray25',highlightthickness=0)
+        s.configure('TScrollbar', background="gray22", foreground="white")
+        s.map('TScrollbar', background=[('active', '!disabled', 'gray22'), ('pressed', "white")],
+              foreground=[('pressed', "white"), ('active', "gray22")])
+        self.configure(background='gray20')
+        self.l_canvas = tk.Canvas(self, background='gray20',highlightthickness=0)
         self.l_canvas.grid(row=0, column=0, columnspan=5)
         self.lesson_frame = tk.Frame(self.l_canvas, width=770,
                                     height=300,
-                                    background='gray25')
+                                    background='gray20')
         self.scrollbar = ttk.Scrollbar(self, style='TScrollbar')
         self.l_canvas.config(yscrollcommand=self.scrollbar.set)
         self.l_canvas.create_window((0, 0), window=self.lesson_frame, anchor='nw')
@@ -23,19 +23,19 @@ class MagicLessonList(tk.Toplevel):
         self.scrollbar.config(command=self.l_canvas.yview)
         self.scrollbar.grid(row=0, column=5, sticky="nsew")
         self.lesson_view_label = ttk.Label(self, text="Lessons View",
-                                      font=("Comic Sans", 14, 'bold'), background="gray27", foreground="white")
+                                      font=("Comic Sans", 14, 'bold'), background="gray22", foreground="white")
         self.scroll_frame = ttk.Frame(self)
         self.headerlesson_id_label = ttk.Label(self.lesson_frame, text="ID", font=('helvetica', 16),
-                                           background='gray25',
+                                           background='gray20',
                                            foreground='aquamarine')
-        self.headerlessonlabel = ttk.Label(self.lesson_frame, text="Lesson Name", font=('helvetica', 16), background='gray25',
+        self.headerlessonlabel = ttk.Label(self.lesson_frame, text="Lesson Name", font=('helvetica', 16), background='gray20',
                                          foreground='aquamarine')
         self.headerfactlabel = ttk.Label(self.lesson_frame, text="Lesson Terms", font=('helvetica', 16),
-                                          background='gray25', foreground='aquamarine')
+                                          background='gray20', foreground='aquamarine')
         self.headerstepslabel = ttk.Label(self.lesson_frame, text="Lesson Skills\n(first step)", font=('helvetica', 16),
-                                           background='gray25', foreground='aquamarine')
+                                           background='gray20', foreground='aquamarine')
         self.headersteps_numberlabel = ttk.Label(self.lesson_frame, text="Skill Steps", font=('helvetica', 16),
-                                          background='gray25', foreground='aquamarine')
+                                          background='gray20', foreground='aquamarine')
 
         self.headerlesson_id_label.grid(row=0, column=0,padx=20, pady=10,sticky=tk.W)
         self.headerlessonlabel.grid(row=0, column=1 ,padx=20,pady=10,sticky=tk.W)
@@ -47,7 +47,7 @@ class MagicLessonList(tk.Toplevel):
         row_index=1
         for element in self.lesson_list:
 
-            bgcolor = "gray25"
+            bgcolor = "gray20"
             self.dataidlabel = ttk.Label(self.lesson_frame, text=element[0], font=('helvetica', 12),
                                            foreground='white', wraplength=200, background=bgcolor)
             self.datanamelabel = ttk.Label(self.lesson_frame, text=element[1], font=('helvetica', 12),
@@ -76,7 +76,7 @@ class MagicLessonList(tk.Toplevel):
 
 # if __name__== "__main__":
 #     dashboard_app = tk.Tk()
-#     dashboard_app.configure(background="gray25")
+#     dashboard_app.configure(background="gray20")
 #     dashboard_app.title("Learning Room Lesson List")
 #     dashboard_app.geometry("1200x800")
 #     frame = MagicLessonList(dashboard_app)
