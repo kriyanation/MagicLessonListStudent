14397b144441ac59199d1b2aeeb0d7f60d499f5d
kriyanation <homeofmars@gmail.com>
2020-08-14 20:38:59 +0530

    sharing Lessons

--- Lesson_List_Display.py
+++ Lesson_List_Display.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import data_capture_lessons
+import sharelesson
 
 
 class MagicLessonList(tk.Toplevel):
@@ -10,10 +11,14 @@ class MagicLessonList(tk.Toplevel):
         s.configure('TScrollbar', background="gray22", foreground="white")
         s.map('TScrollbar', background=[('active', '!disabled', 'gray22'), ('pressed', "white")],
               foreground=[('pressed', "white"), ('active', "gray22")])
+        s.configure('Firebrick.TButton', background='steel blue', foreground='white', font=('helvetica', 12, 'bold'))
+
+        s.map('Firebrick.TButton', background=[('active', '!disabled', 'dark turquoise'), ('pressed', 'white')],
+              foreground=[('pressed', 'white'), ('active', 'white')])
         self.configure(background='gray20')
         self.l_canvas = tk.Canvas(self, background='gray20',highlightthickness=0)
         self.l_canvas.grid(row=0, column=0, columnspan=5)
-        self.lesson_frame = tk.Frame(self.l_canvas, width=770,
+        self.lesson_frame = tk.Frame(self.l_canvas, width=870,
                                     height=300,
                                     background='gray20')
         self.scrollbar = ttk.Scrollbar(self, style='TScrollbar')
@@ -36,15 +41,24 @@ class MagicLessonList(tk.Toplevel):
                                            background='gray20', foreground='aquamarine')
         self.headersteps_numberlabel = ttk.Label(self.lesson_frame, text="Skill Steps", font=('helvetica', 16),
                                           background='gray20', foreground='aquamarine')
+        self.headersteps_sharelabel = ttk.Label(self.lesson_frame, text="Share Lesson", font=('helvetica', 16),
+                                                 background='gray20', foreground='aquamarine')
+        self.headersteps_accesslabel = ttk.Label(self.lesson_frame, text="Share Access", font=('helvetica', 16),
+                                                 background='gray20', foreground='aquamarine')
 
         self.headerlesson_id_label.grid(row=0, column=0,padx=20, pady=10,sticky=tk.W)
         self.headerlessonlabel.grid(row=0, column=1 ,padx=20,pady=10,sticky=tk.W)
         self.headerfactlabel.grid(row=0, column=2,padx=20,pady=10,sticky=tk.W)
         self.headerstepslabel.grid(row=0, column=3, padx=20,pady=10,sticky=tk.W)
         self.headersteps_numberlabel.grid(row=0, column=4,padx=20, pady=10,sticky=tk.W)
+        self.headersteps_sharelabel.grid(row=0, column=5, padx=20, pady=10, sticky=tk.W)
+        self.headersteps_accesslabel.grid(row=0, column=6, padx=20, pady=10, sticky=tk.W)
+        row_index = 1
+        self.share_image = tk.PhotoImage(file="../images/share.png")
+
 
         self.lesson_list = data_capture_lessons.get_Lessons()
-        row_index=1
+
         for element in self.lesson_list:
 
             bgcolor = "gray20"
@@ -62,15 +76,127 @@ class MagicLessonList(tk.Toplevel):
                                            text=str(element[6]),
                                            font=('helvetica', 12),
                                            foreground='white', wraplength=200, background=bgcolor)
+            lesson_id = element[0]
+            self.share_button = ttk.Button(self.lesson_frame, text="", image=self.share_image,
+                                           style="dash.TButton", width=5,
+                                           command=lambda c=lesson_id: self.launch_share(c))
+            text_access = self.get_access_text(lesson_id)
+            self.access_label = ttk.Label(self.lesson_frame,
+                                             text=text_access,
+                                             font=('helvetica', 12),
+                                             foreground='white', wraplength=200, background=bgcolor)
+
             self.dataidlabel.grid(row=row_index,pady=20,column=0,padx=20,sticky=tk.W)
             self.datanamelabel.grid(row=row_index,pady=20, column=1,padx=20,sticky=tk.W)
             self.datafactlabel.grid(row=row_index,pady=20, column=2,padx=20,sticky=tk.W)
             self.datasteplabel.grid(row=row_index,pady=20, column=3,padx=20,sticky=tk.W)
             self.datastepnumbers.grid(row=row_index,pady=20, column=4,padx=20,sticky=tk.W)
+            self.share_button.grid(row=row_index,pady=20, column=5,padx=20,sticky=tk.W)
+            self.access_label.grid(row=row_index, pady=20, column=6, padx=20, sticky=tk.W)
             row_index += 1
+        self.import_button = ttk.Button(self, text="Import a Lesson", width=20,
+                   command=self.import_lesson,
+                   style="Firebrick.TButton")
+        self.import_button.grid(row=1,column=0,columnspan=8,pady=20)
+    def get_access_text(self,lesson_id):
+        if data_capture_lessons.is_shared(lesson_id) == 1:
+            class_id, User = data_capture_lessons.get_user_classid()
+            return ("Shared Access:\nLesson ID = "+str(lesson_id)+"\nClass ID = "+str(class_id)+"\nUser = "+str(User))
+        else:
+            return ""
 
+    def import_lesson(self):
+        self.import_screen = tk.Toplevel(self)
+        self.import_screen.title("Import Lesson")
+        self.import_screen.geometry("350x250")
+        self.import_screen.configure(background="grey20")
+        user = tk.StringVar()
+        classid = tk.StringVar()
+        lessonid = tk.StringVar()
+
+        user_label = tk.Label(self.import_screen, text="Teacher ID", font=("Comic Sans", 12, 'bold'),
+                                  background="gray22", foreground="white")
+        user_label.pack()
+        # Set username entry
+        # The Entry widget is a standard Tkinter widget used to enter or display a single line of text.
+        user_entry = tk.Entry(self.import_screen, textvariable=user)
+        user_entry.pack()
+
+        # Set password label
+        class_label = tk.Label(self.import_screen, text="Class ID", font=("Comic Sans", 12, 'bold'),
+                                  background="gray22", foreground="white")
+        class_label.pack()
+
+        # Set password entry
+        class_entry = tk.Entry(self.import_screen, textvariable=classid)
+        class_entry.pack()
+        lesson_label = tk.Label(self.import_screen, text="Lesson ID", font=("Comic Sans", 12, 'bold'),
+                               background="gray22", foreground="white")
+        lesson_label.pack()
+
+        # Set password entry
+        lesson_entry = tk.Entry(self.import_screen, textvariable=lessonid)
+        lesson_entry.pack()
+
+        self.importstatusvar = tk.StringVar()
+        status_lbl = tk.Label(self.import_screen, textvariable=self.importstatusvar, font=("Comic Sans", 8, 'bold'),
+                              background="gray20", foreground="aquamarine")
+        status_lbl.pack()
+        # Set register button
+        ttk.Button(self.import_screen, text="Start Import", width=10,
+                   command=lambda: self.get_lesson(user.get(), classid.get(), lessonid.get()),
+                   style="Firebrick.TButton").pack()
+    def get_lesson(self,user,classid,lessonid):
+        status = sharelesson.import_new_lesson(user,classid,lessonid)
+        if status == 'error':
+            self.importstatusvar.set("Access information could be wrong, please try again")
+        else:
+            self.import_screen.destroy()
     def l_function(self,event):
-       self.l_canvas.configure(scrollregion=self.l_canvas.bbox("all"),width=1180,height=750)
+       self.l_canvas.configure(scrollregion=self.l_canvas.bbox("all"),width=1380,height=750)
+
+    def launch_share(self,lesson_id):
+
+        self.login_screen = tk.Toplevel(self)
+        self.login_screen.title("Login")
+        self.login_screen.geometry("350x250")
+        self.login_screen.configure(background="grey20")
+        username = tk.StringVar()
+        password = tk.StringVar()
+       # Set username label
+        username_lable = tk.Label(self.login_screen, text="Username * ",font=("Comic Sans", 12, 'bold'), background="gray22", foreground="white")
+        username_lable.pack()
+       # Set username entry
+        # The Entry widget is a standard Tkinter widget used to enter or display a single line of text.
+        username_entry = tk.Entry(self.login_screen, textvariable=username)
+        username_entry.pack()
+
+        # Set password label
+        password_lable = tk.Label(self.login_screen, text="Password * ",font=("Comic Sans", 12, 'bold'), background="gray22", foreground="white")
+        password_lable.pack()
+
+        # Set password entry
+        password_entry = tk.Entry(self.login_screen, textvariable=password, show='*')
+        password_entry.pack()
+        self.statusvar = tk.StringVar()
+        status_lbl = tk.Label(self.login_screen, textvariable=self.statusvar,font=("Comic Sans", 8, 'bold'), background="gray20", foreground="aquamarine")
+        status_lbl.pack()
+        # Set register button
+        ttk.Button(self.login_screen, text="Login", width=10, command = lambda: self.get_token(username.get(),password.get(),lesson_id) ,style="Firebrick.TButton").pack()
+
+
+    def get_token(self,user,pwd,lesson_id):
+        self.logintoken = sharelesson.get_token(user, pwd)
+        if self.logintoken=="error":
+            self.statusvar.set("Login Failed. Please try again or contact support")
+        else:
+            self.login_screen.destroy()
+            self.post_lesson(self.logintoken,lesson_id)
+
+    def post_lesson(self,token,lesson_id):
+        data = sharelesson.prepare_lesson_share(lesson_id)
+        sharelesson.post_lesson(data,token,lesson_id)
+
 
 
 
--- data_capture_lessons.py
+++ data_capture_lessons.py
@@ -1,6 +1,7 @@
 import configparser
 import os
 import sqlite3
+import traceback
 from pathlib import Path
 from tkinter import messagebox
 
@@ -20,3 +21,73 @@ def get_Lessons():
     connection.commit()
     connection.close()
     return list_lessons
+
+
+def get_lessons_for_share(lesson_id):
+    connection = sqlite3.connect(db)
+    cur = connection.cursor()
+    sql = "select Lesson_ID, Lesson_Title, Title_Image,Title_Video,Title_Running_Notes,Factual_Term1,Factual_Term1_Description,Factual_Image1,Factual_Term2,Factual_Term2_Description,Factual_Image2,Factual_Term3,Factual_Term3_Description,Factual_Image3," \
+          "Application_Steps_Number,Application_Step_Description_1,Application_Step_Description_2,Application_Step_Description_3,Application_Step_Description_4,Application_Step_Description_5,Application_Step_Description_6,Application_Step_Description_7," \
+          "Application_Step_Description_8,Application_Steps_Widget_1,Application_Steps_Widget_2,Application_Steps_Widget_3,Application_Steps_Widget_4,Application_Steps_Widget_5,Application_Steps_Widget_6,Application_Steps_Widget_7," \
+          "Application_Steps_Widget_8,IP_Questions,Apply_External_Link from Magic_Science_Lessons where Lesson_ID=?"
+    cur.execute(sql,(lesson_id,))
+    rows = cur.fetchall()
+    connection.commit()
+    connection.close()
+    return rows[0]
+
+def get_user_classid():
+    connection = sqlite3.connect(db)
+    cur = connection.cursor()
+    sql = "select class_id, User from Magic_Teacher_Data where Class_No=?"
+    cur.execute(sql,(1,))
+    rows = cur.fetchall()[0]
+    connection.commit()
+    connection.close()
+    return rows[0],rows[1]
+
+
+def update_shared(lesson_id):
+    connection = sqlite3.connect(db)
+    cur = connection.cursor()
+    sql = "update Magic_Science_Lessons set Shared_Flag=1 where Lesson_ID=?"
+    cur.execute(sql, (lesson_id,))
+    connection.commit()
+    connection.close()
+
+def is_shared(lesson_id):
+    connection = sqlite3.connect(db)
+    cur = connection.cursor()
+    sql = "select Shared_Flag from Magic_Science_Lessons where Lesson_ID=?"
+    cur.execute(sql, (lesson_id,))
+    rows = cur.fetchone()
+    connection.commit()
+    connection.close()
+    return rows[0]
+
+
+def insert_imported_record(query_parameters):
+    try:
+        connection = sqlite3.connect(db)
+        cur = connection.cursor()
+        sql = ('insert into Magic_Science_Lessons (Lesson_Title,Title_Image,Title_Video,Title_Running_Notes,Factual_Term1,Factual_Term1_Description,Factual_Image1,Factual_Term2,Factual_Term2_Description,Factual_Image2,Factual_Term3,Factual_Term3_Description,'
+                       'Factual_Image3,Application_Steps_Number,Application_Step_Description_1,Application_Steps_Widget_1,Application_Step_Description_2,Application_Steps_Widget_2,Application_Step_Description_3,Application_Steps_Widget_3,Application_Step_Description_4,Application_Steps_Widget_4,'
+                       'Application_Step_Description_5,Application_Steps_Widget_5, Application_Step_Description_6,Application_Steps_Widget_6, Application_Step_Description_7, Application_Steps_Widget_7,Application_Step_Description_8,Application_Steps_Widget_8,'
+                       'IP_Questions) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)')
+        cur.execute(sql, query_parameters)
+        connection.commit()
+        connection.close()
+    except (Exception):
+        traceback.print_exc()
+def get_new_id():
+    try:
+        connection = sqlite3.connect(db)
+        cur = connection.cursor()
+        sql = "SELECT seq FROM sqlite_sequence where name = 'Magic_Science_Lessons'"
+        cur.execute(sql)
+        rows = cur.fetchone()
+        new_id = rows[0]
+        return new_id
+    except sqlite3.OperationalError:
+        traceback.print_exc()
+
